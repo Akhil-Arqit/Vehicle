219a10e050014ae722c6d9ead3bcbe80dd2d0d55
Akhil <gakhil@ariqt.com>
2025-01-08 12:25:35 +0530

    refactor

--- app/src/main/java/com/example/vehicle/view/MainActivity.kt
+++ app/src/main/java/com/example/vehicle/view/MainActivity.kt
@@ -9,14 +9,21 @@ import androidx.compose.animation.core.EaseInOutCubic
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -29,12 +36,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextGeometricTransform
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.vehicle.R
 import com.example.vehicle.common.DataViewModelFactory
 import com.example.vehicle.common.UIState
 import com.example.vehicle.model.Data
@@ -48,6 +61,7 @@ import ir.ehsannarmani.compose_charts.models.AnimationMode
 import ir.ehsannarmani.compose_charts.models.DrawStyle
 import ir.ehsannarmani.compose_charts.models.HorizontalIndicatorProperties
 import ir.ehsannarmani.compose_charts.models.LabelHelperProperties
+import ir.ehsannarmani.compose_charts.models.LabelProperties
 import ir.ehsannarmani.compose_charts.models.Line
 import ir.ehsannarmani.compose_charts.models.Pie
 import kotlin.random.Random
@@ -69,11 +83,112 @@ class MainActivity : ComponentActivity() {
                 ) { innerPadding ->
                     LazyColumn(
                         modifier = Modifier.padding(innerPadding),
-                        horizontalAlignment = Alignment.CenterHorizontally,
+                        horizontalAlignment = Alignment.Start,
                         verticalArrangement = Arrangement.Center
                     ) {
                         item {
-                            Spacer(modifier = Modifier.height(50.dp))
+                            Spacer(modifier = Modifier.height(15.dp))
+                            Text(
+                                modifier = Modifier.fillMaxWidth(),
+                                fontFamily = FontFamily(
+                                    Font(R.font.electrolize_regular, FontWeight.W900)
+                                ),
+                                fontSize = 28.sp,
+                                textAlign = TextAlign.Center,
+                                text = "Total EV Sales",
+                                style = MaterialTheme.typography.titleLarge
+                            )
+                            Text(
+                                modifier = Modifier.fillMaxWidth(),
+                                fontFamily = FontFamily(
+                                    Font(R.font.zcool, FontWeight.Medium)
+                                ),
+                                fontSize = 42.sp,
+                                letterSpacing = 3.sp,
+                                textAlign = TextAlign.Center,
+                                text = "1287641",
+                                style = MaterialTheme.typography.titleLarge
+                            )
+                            Spacer(modifier = Modifier.height(12.dp))
+                            Text(
+                                modifier = Modifier.padding(
+                                    start = 15.dp,
+                                ),
+                                fontFamily = FontFamily(
+                                    Font(R.font.electrolize_regular, FontWeight.Medium)
+                                ),
+                                fontSize = 14.sp,
+                                text = "In Sep. '24",
+                                style = MaterialTheme.typography.titleLarge
+                            )
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceBetween
+                            ) {
+                                Text(
+                                    modifier = Modifier.padding(
+                                        start = 15.dp,
+                                        bottom = 20.dp
+                                    ),
+                                    fontFamily = FontFamily(
+                                        Font(R.font.zcool, FontWeight.Medium)
+                                    ),
+                                    letterSpacing = 3.sp,
+                                    fontSize = 36.sp,
+                                    text = "396175",
+                                    style = MaterialTheme.typography.titleLarge
+                                )
+                                Text(
+                                    modifier = Modifier.padding(
+                                        top = 12.dp,
+                                        start = 5.dp,
+                                    ).weight(1f),
+                                    textAlign = TextAlign.Start,
+                                    letterSpacing = 2.sp,
+                                    fontSize = 14.sp,
+                                    text = "EV's Sold",
+                                    style = MaterialTheme.typography.titleLarge
+                                )
+                                Row(
+                                    modifier = Modifier
+                                        .wrapContentWidth().padding(end = 12.dp),
+                                    horizontalArrangement = Arrangement.End
+                                ) {
+                                    Text(
+                                        modifier = Modifier
+                                            .padding(
+                                                start = 5.dp,
+                                            ),
+                                        fontFamily = FontFamily(
+                                            Font(R.font.zcool, FontWeight.Normal)
+                                        ),
+                                        textAlign = TextAlign.End,
+                                        letterSpacing = 2.sp,
+                                        fontSize = 28.sp,
+                                        text = "8.99",
+                                        style = MaterialTheme.typography.titleLarge
+                                    )
+                                    Image(
+                                        modifier = Modifier.size(18.dp).padding(start = 5.dp),
+                                        contentDescription = null,
+                                        painter = painterResource(R.drawable.up_arrow)
+                                    )
+                                }
+                            }
+
+                            Row(
+                                horizontalArrangement = Arrangement.Center,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                PieChartSample()
+                                Image(
+                                    modifier = Modifier.size(100.dp).offset(x= (-50).dp).scale(2.7f,2.7f).weight(1f),
+                                    contentDescription = null,
+                                    painter = painterResource(R.drawable.car)
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(70.dp))
                             when (uiState) {
                                 is UIState.Loading -> {
                                     Column(
@@ -96,9 +211,6 @@ class MainActivity : ComponentActivity() {
                                         vehicleData.data.dropLast(1)
                                     )
                                     Spacer(modifier = Modifier.height(100.dp))
-                                    PieChartSample(
-                                        modifier = Modifier.padding(innerPadding),
-                                    )
                                 }
                                 is UIState.Error -> {
                                     val errorMessage = (uiState as UIState.Error).message
@@ -115,47 +227,57 @@ class MainActivity : ComponentActivity() {
 
 
     @Composable
-    fun PieChartSample(modifier: Modifier = Modifier) {
+    fun PieChartSample() {
         var data by remember {
             mutableStateOf(
                 listOf(
                     Pie(
                         label = "Android",
                         data = 20.0,
-                        color = Color.Red,
-                        selectedColor = Color.Green
+                        color = Color(0xFFFF6384).copy(alpha = 0.5f),
+                        selectedColor = Color(0xFFFF6384)
                     ),
                     Pie(
                         label = "Windows",
                         data = 45.0,
-                        color = Color.Cyan,
-                        selectedColor = Color.Blue
+                        color = Color(0xFF36A2EB).copy(alpha = 0.5f),
+                        selectedColor = Color(0xFF36A2EB)
                     ),
                     Pie(
                         label = "Linux",
                         data = 35.0,
-                        color = Color.Gray,
-                        selectedColor = Color.Yellow
+                        color = Color(0xFFFFCE56).copy(alpha = 0.5f),
+                        selectedColor = Color(0xFFFFCE56)
+                    ),
+                    Pie(
+                        label = "Windows",
+                        data = 45.0,
+                        color = Color(0xFF4BC0C0).copy(alpha = 0.5f),
+                        selectedColor = Color(0xFF4BC0C0)
+                    ),
+                    Pie(
+                        label = "Linux",
+                        data = 35.0,
+                        color = Color(0xFF9966FF).copy(alpha = 0.5f),
+                        selectedColor = Color(0xFF9966FF)
                     ),
                 )
             )
         }
-
+        var selectedText = ""
         PieChart(
-            modifier = Modifier
-                .size(400.dp)
-                .padding(50.dp),
+            modifier = Modifier.width(250.dp).height(150.dp).offset(x= (-30).dp),
             data = data,
             onPieClick = {
+                selectedText = it.label.toString()
                 println("${it.label} Clicked")
                 val pieIndex = data.indexOf(it)
-                data =
-                    data.mapIndexed { mapIndex, pie -> pie.copy(selected = pieIndex == mapIndex) }
+                data = data.mapIndexed { mapIndex, pie -> pie.copy(selected = pieIndex == mapIndex) }
             },
             selectedScale = 1.05f,
             spaceDegree = 1f,
             selectedPaddingDegree = 0f,
-            style = Pie.Style.Stroke(width = 50.dp),
+            style = Pie.Style.Stroke(width = 20.dp),
             scaleAnimEnterSpec = spring(
                 dampingRatio = Spring.DampingRatioMediumBouncy,
                 stiffness = Spring.StiffnessLow
@@ -165,19 +287,31 @@ class MainActivity : ComponentActivity() {
             scaleAnimExitSpec = tween(300),
             spaceDegreeAnimExitSpec = tween(300),
         )
+        Text(
+            modifier = Modifier.offset(x = (-180).dp).width(100.dp),
+            fontFamily = FontFamily(
+                Font(R.font.zcool, FontWeight.Normal)
+            ),
+            textAlign = TextAlign.Start,
+            letterSpacing = 2.sp,
+            fontSize = 28.sp,
+            text = "8.99",
+            style = MaterialTheme.typography.titleLarge
+        )
     }
+
 
     @Composable
     fun LineChartSample(modifier: Modifier = Modifier, data: List<Data>) {
         LineChart(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(270.dp)
+                .height(250.dp)
                 .padding(horizontal = 22.dp),
             data = remember {
                 data.map { data ->
                     Line(
-                        label = data.category,
+                        label = if (data.category == "3 Wheeler (Passenger)") "3 Wheeler" else data.category,
                         values = data.y.map { it.toDouble() / 1000},
                         color = SolidColor(Color(
                             red = Random.nextFloat(),
@@ -199,13 +333,18 @@ class MainActivity : ComponentActivity() {
             labelHelperProperties = LabelHelperProperties(
                 textStyle = TextStyle.Default.copy(
                     color = MaterialTheme.colorScheme.onSurface,
-                    fontSize = 12.sp
+                    fontSize = 13.sp
                 ),
             ),
             indicatorProperties = HorizontalIndicatorProperties(
                 textStyle = TextStyle.Default.copy(color = MaterialTheme.colorScheme.onSurface),
                 padding = 16.dp
             ),
+            labelProperties = LabelProperties(
+                enabled = true,
+                padding = 10.dp,
+                textStyle = TextStyle.Default.copy(color = MaterialTheme.colorScheme.onSurface),
+                labels = data[0].x.map { it.toString() }),
         )
     }
 }
